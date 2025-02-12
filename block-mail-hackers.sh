29899be25579fff5e58a017ac083d78d725f1dff
Your Name <you@example.com>
2025-02-12 09:43:26 +0000

    Did some simple updates to the look of code

--- block-mail-hackers.sh
+++ block-mail-hackers.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# To regularly scan maillog and block brute force attacks
 
-# make file of iptables rules
+# To regularly scan maillog and block brute force attacks
+# I run this under cron every hour
+
+# Make a file of iptables rules
 /usr/sbin/iptables -L -n > /root/iptables_output
 
-# Get a list of ip addresses
-IP_ADDRESSES=`/usr/bin/cat  /var/log/maillog | /usr/bin/grep 'authentication failed' | /usr/bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /usr/bin/sort -n `
+# Get a list of ip addresses of failed authentication from mail log
+IP_ADDRESSES=`/usr/bin/cat  /var/log/maillog | /usr/bin/grep 'authentication failed' \
+ | /usr/bin/grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' | /usr/bin/sort -n `
+
 # List the unique ip addresses
 UNIQUE_IP_ADDRESSES=`echo "$IP_ADDRESSES" | uniq `
 
@@ -17,14 +21,8 @@ do
     COUNT=`/usr/bin/grep "$i" /var/log/maillog | wc -l ` 
     if [ "$COUNT" -gt 5 ]
     then
-    ## We now need to insert into iptables if it not already there
+         ## We now need to insert into iptables if it not already there
          /usr/bin/grep "$i" /root/iptables_output || /usr/sbin/iptables -I INPUT 1  -s $i  -j DROP         
-    
-
     fi 
-
-
-
- 
 done
 
